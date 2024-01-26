abb90eabe23b91d1c91d75ae8f85de20b12a210b
Roman Ortega Mondaca <romtega88@gmail.com>
2024-01-25 20:22:53 -0600

    basic setup and utility classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Home from "./pages/Home/";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>
+        <Home />
+      </h1>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../../components/";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      Home
+    </div>
+  );
+};
+
+export default Home;
