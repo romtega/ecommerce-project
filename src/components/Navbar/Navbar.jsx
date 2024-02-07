9fa2a82c5c7185a19a3949c1acda735dc3077813
Roman Ortega Mondaca <romtega88@gmail.com>
2024-02-07 01:22:37 -0600

    changed color scheme and added ion-icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,36 +2,44 @@ import SearchIcon from "@/assets/search-icon.svg";
 import ShoppingCartIcon from "@/assets/shopping-cart-icon.svg";
 import HeartIcon from "@/assets/hearticon.svg";
 import HelpIcon from "@/assets/helpicon.svg";
+import UserIcon from "@/assets/usericon.svg";
 import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="navbar flex flex-center bg-dark text-accent">
-      <img src={ShoppingCartIcon} alt="" className="logo" />
-      <ul className="flex flex-center">
-        <li>Categorias</li>
-        <li>Ingresa a tu cuenta</li>
-        <li>
-          <a href="">
-            <img src={HelpIcon} alt="" className="logo" />
-          </a>
-        </li>
-      </ul>
+    <div className="main-navbar flex flex-center container bg-accent text-white">
+      <div className="main-navbar-brand uppercase letter-spacing-1 fs-500 fw-bold">
+        ikomers
+      </div>
       <ul className="flex flex-center">
         <li>
           <a href="">
-            <img src={ShoppingCartIcon} alt="" className="logo" />
+            <ion-icon
+              name="person-circle-outline"
+              className="main-navbar-icon"
+            ></ion-icon>
           </a>
         </li>
         <li>
           <a href="">
-            <img src={HeartIcon} alt="" className="logo" />
+            <ion-icon
+              name="cart-outline"
+              className="main-navbar-icon"
+            ></ion-icon>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <ion-icon
+              name="heart-outline"
+              className="main-navbar-icon"
+            ></ion-icon>
           </a>
         </li>
         <li>
           <div className="search-box flex flex-center">
             <input type="text" placeholder="Busca un producto" />
-            <img src={SearchIcon} alt="Search Icon" className="icon" />
+            <ion-icon name="search-outline"></ion-icon>
           </div>
         </li>
       </ul>
