8aef3a18b998f099324f7f3732529c41a35c88f4
Roman Ortega Mondaca <romtega88@gmail.com>
2024-02-20 14:31:22 -0600

    navbar login logout features

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
 import { NavLink } from "react-router-dom";
 import useItemsContext from "@/hooks/useItemsContext";
+import { AuthContext } from "@/context/AuthContext";
 import "./navbar.css";
+import useAuthContext from "../../hooks/useAuthContext";
 
 const Navbar = () => {
+  const { isAuth, logout } = useAuthContext();
+
   const { searchTerm, setSearchTerm } = useItemsContext();
 
   const handleSearch = (e) => {
@@ -16,11 +20,6 @@ const Navbar = () => {
       </div>
       <ul className="main-navbar-list flex flex-center">
         <li>
-          <NavLink to="/loginform" className="main-navbar-icon flex text-white">
-            <ion-icon name="person-circle-outline"></ion-icon>
-          </NavLink>
-        </li>
-        <li>
           <NavLink
             to="/shoppingcart"
             className="main-navbar-icon flex text-white"
@@ -33,6 +32,37 @@ const Navbar = () => {
             <ion-icon name="heart-outline"></ion-icon>
           </NavLink>
         </li>
+        {isAuth ? (
+          <>
+            <li>
+              <NavLink
+                to="/loginform"
+                className="main-navbar-icon flex bg-light text-dark"
+              >
+                <ion-icon name="person"></ion-icon>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/"
+                onClick={logout}
+                className="main-navbar-icon flex bg-warning text-dark"
+              >
+                <ion-icon name="log-out-outline"></ion-icon>
+              </NavLink>
+            </li>
+          </>
+        ) : (
+          <li>
+            <NavLink
+              to="/loginform"
+              className="main-navbar-icon flex text-white"
+            >
+              <ion-icon name="person-circle-outline"></ion-icon>
+            </NavLink>
+          </li>
+        )}
+
         <li>
           <div className="main-navbar-search flex flex-center">
             <input
